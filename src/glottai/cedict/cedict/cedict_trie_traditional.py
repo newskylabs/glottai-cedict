31a81879b0dde9023c4f58c875c6a30497c49e24
newskylabs <dietrich@newskylabs.net>
2023-05-17 22:20:00 +0200

    Added an example cedict.txt file with only four entries and the corresponding compiled trie files.

---
+++ src/glottai/cedict/cedict/cedict_trie_traditional.py
@@ -0,0 +1,40 @@
+# CC-CEDICT Python trie
+# 
+# Generated from the CC-CEDICT text file published by MDBG
+# 
+# Original header of the CC-CEDICT text file:
+# --
+# CC-CEDICT
+# Community maintained free Chinese-English dictionary.
+#
+# Example file with only 4 entries
+#
+# --
+
+CEDICT_variables = {
+  'version': '0',
+  'subversion': '0',
+  'format': 'ts',
+  'charset': 'UTF-8',
+  'entries': '4',
+  'publisher': 'MDBG',
+  'license': 'https://creativecommons.org/licenses/by-sa/4.0/',
+  'date': '2023-05-14T06:17:53Z',
+  'time': '1684045073'
+}
+
+CEDICT_trie = {'子': {True:
+# 子
+'子 子 [zǐ] /son/child/seed/egg/small thing/1st earthly branch: 11 p.m.-1 a.m., midnight, 11th solar month (7th December to 5th January), year of the Rat/Viscount, fourth of five orders of nobility 五等爵位[wǔ děng jué wèi]/ancient Chinese compass point: 0° (north)/subsidiary/subordinate/sub-/'
+}, '小': {True:
+# 小
+'小 小 [xiǎo] /small/tiny/few/young/'
+}, '王': {True:
+# 王
+'王 王 [wáng] /king or monarch/best or strongest of its type/grand/great/'
+, '子': {True:
+# 王子
+'王子 王子 [wáng zǐ] /prince/son of a king/'
+}}}
+
+# fin.
